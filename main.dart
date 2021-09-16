d12fd532088808a32fe100515d0cd367cf7d4e4d
noviaarfitri13 <80112122+noviaarfitri13@users.noreply.github.com>
2021-09-16 14:47:24 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class Tampilan extends StatelessWidget {
+Widget build(BuildContext context) {
+return MaterialApp(
+home: Scaffold(
+appBar: AppBar(
+title: Text('Sample Code'),
+
+leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+),
+body: Center(child: Text('Hallo')),
+backgroundColor: Colors.blueGrey.shade200,
+));
+}
+}
+void main() {
+runApp(Tampilan());
+}
